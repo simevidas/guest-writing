068d4f8d518938a5d4c4dfc449aa9f36bb524538
Šime Vidas <sime.vidas@gmail.com>
2024-02-12 20:47:15 +0100

    various improvements and fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({
-    './public/': '/',
-  });
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPassthroughCopy({ './public/': '/' });
+  eleventyConfig.addPassthroughCopy('**/*.{jpg,jpeg,png,gif,mp4,webm}');
   eleventyConfig.addWatchTarget('./public/styles.css');
 };
